6deb4c87c01588cb6f507bf4189fb1e867900104
Kaarlat <trujillo.karla@hotmail.com>
2024-05-16 20:32:46 -0400

    inicio primera preentrega

---
+++ ecommerce-kt/src/components/navbar/navbar.jsx
@@ -0,0 +1,15 @@
+import { Button } from "antd"
+
+const Navbar =() => {
+    return(
+        <nav>
+            <p>TrimaKer</p>
+            <button>Inicio</button>
+            <button>Categorías</button>
+            <button>Contacto</button>
+            <cartwidget/>
+        </nav>
+    )
+}
+
+export default Navbar
